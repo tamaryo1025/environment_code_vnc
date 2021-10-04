7c0e475d927397b19061d9484528cdf8d79ea1e1
tamaryo1025 <tamaryo1025@gmail.com>
2021-10-05 02:25:07 +0900

    [modify] imageの名前の修正

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 8888:8888 -v /home/tamaki:/home/tamaki/workdir/ --name tamaki_env vnc
+docker run -it -p 8888:8888 -v /home/tamaki:/home/tamaki/workdir/ --name tamaki_env rtamaki1025/vscode_vnc
