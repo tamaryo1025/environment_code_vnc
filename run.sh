f76df39db17d6add1b1fb4a39e7de4dd036e3a30
tamaryo1025 <tamaryo1025@gmail.com>
2021-10-05 02:59:36 +0900

    [modify] gpuを有効化

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 8888:8888 -v /home/tamaki:/home/tamaki/workdir/ --name tamaki_env rtamaki1025/vscode_vnc
+docker run -it -p 8888:8888 --gpus all -v /home/tamaki:/home/tamaki/workdir/ --name tamaki_env rtamaki1025/vscode_vnc
